6d1737ded726270b696c8cd8b639eb324b4327ee
kinyarasam <kinyarasam@macs-MacBook-Pro.local>
2023-10-04 16:23:39 +0300

    Get One Customer record

--- controller/customerController.js
+++ controller/customerController.js
@@ -3,7 +3,6 @@ import Customer from "../models/customer";
 
 class CustomerController {
   static createNew(req, res) {
-    console.log(req.params)
     const { firstName } = req.body;
     const { lastName } = req.body;
     const { phoneNumber } = req.body;
@@ -26,31 +25,46 @@ class CustomerController {
       return res.status(400).json({ error: 'Missing lastName'});
     }
 
-    const new_customer = new Customer({
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      phoneNumber: phoneNumber || '',
-      address: {
-        city: city || '',
-        street: street || '',
-        country: country || '',
-        state: state || '',
-        postalCode: postalCode || ''
-      }
-    });
+    return Customer
+      .findOne({ email: email, phoneNumber: phoneNumber, firstName: firstName})
+      .then((customer) => {
 
-    return new_customer.save()
-      .then((record) => {
-        console.log(record)
-        return res.status(201).json({
-            customer: 'record'
-          })
+        if (customer !== null) {
+          return res.status(400).json({ error: 'Account Already exists' })
+        }
+
+        const new_customer = new Customer({
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          phoneNumber: phoneNumber || '',
+          address: {
+            city: city || '',
+            street: street || '',
+            country: country || '',
+            state: state || '',
+            postalCode: postalCode || ''
+          }
+        });
+
+        return new_customer.save()
+        .then((detail) => {
+          return res.status(201).json({
+              customer: {
+                id: detail._id,
+                firstName: detail.firstName,
+                lastName: detail.lastName,
+                email: detail.email,
+                firstName: detail.firstName
+              }
+            })
+        })
+        .catch((error) => {
+          console.log(error)
+          return res.status(401).json({error: `Unable to save the record: ${error}`});
+        });
       })
-      .catch((error) => {
-        console.log(error)
-        return res.status(401).json({error: `Unable to save the record: ${error}`});
-      });
+      .catch((err) => res.status(500).json({error: err}))
   }
 
   static getAll(req, res) {
@@ -68,9 +82,25 @@ class CustomerController {
   }
 
   static getOne(req, res) {
-    // const customer = Customer.find()
-    // console.log(customer);
-    // return res.status(200).json({ msg: 'Working' });
+    const { email, firstName, lastName } = req.body;
+
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+
+    if (!firstName) return res.status(400).json({ error: 'Missing first name' });
+
+    if (!lastName) return res.status(400).json({ error: 'Missing last name' });
+
+    return Customer.findOne({
+      email: email,
+      firstName: firstName,
+      lastName: lastName
+    })
+      .then((customer) => {
+        if (!customer) return res.status(404).json({ error: 'No Customer Found' });
+
+        return res.status(200).json(customer);
+      })
+      .catch((err) => res.status(500).json(err));
   }
 }
 
